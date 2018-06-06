258da33cd5dfaddfad579f3817a087a7fab246be
GOWRI <31369935+GOWRICSE@users.noreply.github.com>
2018-06-06 17:52:30 +0530

    Add files via upload

---
+++ CommandLine_2.java
@@ -0,0 +1,6 @@
+public class CommandLine_2 {
+	public static void main(String aa[])
+	{
+		System.out.println(" Welcome "+aa[0]);
+	}
+}
---
+++ CommandLine_3.java
@@ -0,0 +1,12 @@
+public class CommandLine_3
+{
+	public static void main(String aa[])
+	{
+		int x,y,z;
+		x=Integer.parseInt(aa[0]);
+        y=Integer.parseInt(aa[1]);
+		z=x+y;
+		System.out.println("The sum of "+x+" and "+y+" is "+z);
+		}
+
+}
